a3af8e008c8abb10aa0940ee393e48f15b1f8f47
Diego Tettamanti <tettamanti.diego@gmail.com>
2023-02-01 20:56:29 -0300

    commit9

--- app.js
+++ app.js
@@ -9,24 +9,76 @@ const products = [
     {
      
             "title": "Headphones",
-            "price": 250,
+            "price": 50,
             "thumbnail": "https://cdn3.iconfinder.com/data/icons/vol-3/128/headphones-1024.png",
-            "id": "1"
+            "id": 1
     }
     ,
      {
             "title": "Guitar",
             "price": 450,
             "thumbnail": "https://cdn3.iconfinder.com/data/icons/vol-3/128/guitar-1024.png",
-            "id": "2"
+            "id": 2
     },
     
      {
             "title": "Piano",
-            "price": 750,
+            "price": 4750,
             "thumbnail": "https://media.istockphoto.com/photos/keyboard-music-white-picture-id481907373" ,
-            "id": "3"
-    } ]
+            "id": 3
+    } ,
+
+    {
+            "title": "Saxophone",
+            "price": 1350,
+            "thumbnail": "https://media.istockphoto.com/photos/keyboard-music-white-picture-id481907373" ,
+            "id": 4
+    } ,
+
+    {
+            "title": "Trumpet",
+            "price": 1250,
+            "thumbnail": "https://media.istockphoto.com/photos/keyboard-music-white-picture-id481907373" ,
+            "id": 5
+    } ,
+    {
+            "title": "Harp",
+            "price": 2000,
+            "thumbnail": "https://media.istockphoto.com/photos/keyboard-music-white-picture-id481907373" ,
+            "id": 6
+    } ,
+
+    {
+            "title": "Drums",
+            "price": 550,
+            "thumbnail": "https://media.istockphoto.com/photos/keyboard-music-white-picture-id481907373" ,
+            "id": 7
+    },
+    
+    {
+            "title": "Bandonion",
+            "price": 1750,
+            "thumbnail": "https://media.istockphoto.com/photos/keyboard-music-white-picture-id481907373" ,
+            "id": 8
+    },
+    
+    {
+            "title": "Organ",
+            "price": 800,
+            "thumbnail": "https://media.istockphoto.com/photos/keyboard-music-white-picture-id481907373" ,
+            "id": 9
+    } ,
+
+    {
+            "title": "Bongo",
+            "price": 200,
+            "thumbnail": "https://media.istockphoto.com/photos/keyboard-music-white-picture-id481907373" ,
+            "id": 10
+    } 
+
+
+
+]
 
 
 
@@ -37,7 +89,7 @@ app.get('/', (req, res) => {
     console.log("Devuelve array de productos")})
 
     app.get('/productos/:id', (req, res) => {
-        res.send('/productos.txt')})
+        res.send('/productos.json')})
 
 
 app.get('/api/:id', (req, res) =>{
