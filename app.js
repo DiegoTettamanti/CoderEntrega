a83679783d0380cf840c6620210228542a693c40
Diego <tettamanti.diego@gmail.com>
2023-02-01 20:53:34 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,73 @@
 import  express  from 'express';
-import productManager from './productManager.js';
+
 
 
 const app = express();
 const PORT = 8080
 app.use(express.urlencoded({ extended: true }))
-app.get('/query', (req, res) =>
-{
-    res.send(req.query)
-    console.log("Query sent")
-    const {pruducts: products, id} = req.query
+const products = [
+    {
+     
+            "title": "Headphones",
+            "price": 250,
+            "thumbnail": "https://cdn3.iconfinder.com/data/icons/vol-3/128/headphones-1024.png",
+            "id": "1"
+    }
+    ,
+     {
+            "title": "Guitar",
+            "price": 450,
+            "thumbnail": "https://cdn3.iconfinder.com/data/icons/vol-3/128/guitar-1024.png",
+            "id": "2"
+    },
+    
+     {
+            "title": "Piano",
+            "price": 750,
+            "thumbnail": "https://media.istockphoto.com/photos/keyboard-music-white-picture-id481907373" ,
+            "id": "3"
+    } ]
+
+
+
+//http://localhost:8080/products
+
+app.get('/', (req, res) => {
+    res.send(products)
+    console.log("Devuelve array de productos")})
+
+    app.get('/productos/:id', (req, res) => {
+        res.send('/productos.txt')})
+
+
+app.get('/api/:id', (req, res) =>{
+    const { id } = req.params
+    const product = products.find(product => product.id === id)
+    if(!product) return res.send("product not found")
+    res.send(
+        product
+    )
+    })
+
+app.get('/params/:title/:id', (req, res) => {
+    console.log(req.params);
+    const {title,id} = req.params;
+    res.send({title, id})
+})
+
+
+app.get('/query', (req, res) => {
+    const { title, price, id } = req.query
     res.send({
-        products,
+        title,
+        price,
         id
     })
 })
 
 
-app.get('/productos', (req, res) => {
-    res.send('/productos')
-    console.log("Probando respuesta del servidor...")})
-    app.get('/productos/:id', (req, res) => {
-        res.send('/productos.txt')})
-
-app.listen(PORT, err =>{
-    if(err){
-        console.log(err)
-    }else{`Escuchando en el puerto {PORT} en el servidor`
-}})
-app.get('/params/:products/:id', (req, res) =>
-    console.log(product, id))
-
+    app.listen(PORT, err =>{
+        if(err){
+            console.log(err)
+        }else{`Escuchando en el puerto {PORT} en el servidor`
+    }})
